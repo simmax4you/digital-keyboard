8ab28a761eab35eb0207370f14fa2a6ecd98ce7c
simmax4you <93457125+simmax4you@users.noreply.github.com>
2022-06-12 08:55:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,180 @@
+var alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var num = ["1","2","3","4","5","6","7","8","9","0"];
+var letter = "";
+var output = ""
+function A(){
+    letter = alph[0];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function B(){
+    letter = alph[1];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function C(){
+    letter = alph[2];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function D(){
+    letter = alph[3];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}function E(){
+    letter = alph[4];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}function F(){
+    letter = alph[5];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function G(){
+    letter = alph[6];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}function H(){
+    letter = alph[7];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function I(){
+    letter = alph[8];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function J(){
+    letter = alph[9];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function K(){
+    letter = alph[10];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function L(){
+    letter = alph[11];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function M(){
+    letter = alph[12];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}function N(){
+    letter = alph[13];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function O(){
+    letter = alph[14];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function P(){
+    letter = alph[15];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function Q(){
+    letter = alph[16];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function R(){
+    letter = alph[17];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function S(){
+    letter = alph[18];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function T(){
+    letter = alph[19];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function U(){
+    letter = alph[20];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function V(){
+    letter = alph[21];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function W(){
+    letter = alph[22];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function X(){
+    letter = alph[23];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function Y(){
+    letter = alph[24];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function Z(){
+    letter = alph[25];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function one(){
+    letter = num[0];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function two(){
+    letter = num[1];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function three(){
+    letter = num[2];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function four(){
+    letter = num[3];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function five(){
+    letter = num[4];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function six(){
+    letter = num[5];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function seven(){
+    letter = num[6];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function eight(){
+    letter = num[7];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function nine(){
+    letter = num[8];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
+function zero(){
+    letter = num[9];
+    output = output.concat(letter)
+    document.getElementById("output").innerHTML=output;
+}
